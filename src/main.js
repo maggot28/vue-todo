e02111bd0c993bbb2fc6961a5898ac336ce24109
maggot28 <rybakvlad183@gmail.com>
2020-10-08 16:23:13 +0300

    added main.js, index.html

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+import Axios from 'axios'
+
+import VueI18n from 'vue-i18n'
+import VueMaterial from 'vue-material'
+import Toasted from 'vue-toasted';
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.config.productionTip = false
+
+Vue.use(VueMaterial)
+Vue.use(VueI18n)
+Vue.use(Toasted, {
+  position: "bottom-right",
+  duration: 3000,
+  keepOnHover: true,
+})
+
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem('VD_token')
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = "Bearer " + token;
+}
+
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount('#app')
