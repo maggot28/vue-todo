4da2507614179c66cda58398b14be3787f2d3f59
maggot28 <rybakvlad183@gmail.com>
2020-10-13 13:41:51 +0300

    add drag on scroll plugin

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import Axios from 'axios'
 import VueI18n from 'vue-i18n'
 import VueMaterial from 'vue-material'
 import Toasted from 'vue-toasted';
+import VueDragscroll from 'vue-dragscroll'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
 
+Vue.use(VueDragscroll)
 Vue.use(VueMaterial)
 Vue.use(VueI18n)
 Vue.use(Toasted, {
@@ -20,7 +22,7 @@ Vue.use(Toasted, {
 })
 
 Vue.prototype.$http = Axios;
-const token = localStorage.getItem('VD_token')
+const token = localStorage.getItem('VD_token');
 if (token) {
   Vue.prototype.$http.defaults.headers.common['Authorization'] = "Bearer " + token;
 }
